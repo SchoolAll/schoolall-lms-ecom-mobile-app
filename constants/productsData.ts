26f2960ec458210bab6ad299b4f8c5f175f0c001
halino2710 <kemokaihamed2710@gmail.com>
2024-04-25 23:53:55 +0000

    Add dummy products' data

---
+++ constants/productsData.ts
@@ -0,0 +1,57 @@
+export const products = 
+    [
+        {
+        productId: "1",
+        name: "School bag",
+        price: 290.99,
+        image: require("../assets/images/bag.jpg"),
+        description: "Universal school bag suitable for senior pupils",
+        rating: 4,
+        ratingCount: 65
+    },
+    {
+        productId: "2",
+        name: "School bag (L)",
+        price: 299.99,
+        image: require("../assets/images/bag1.jpg"),
+        description: "Universal school bag suitable for all categories of pupils",
+        rating: 4.4,
+        ratingCount: 50
+    },
+    {
+        productId: "3",
+        name: "Bow tie",
+        price: 45.99,
+        image: require("../assets/images/bowTie.jpg"),
+        rating: 5,
+        ratingCount: 12,
+        description: "This is the official bow-tie for the male pupils"
+    },
+    {
+        productId: "4",
+        name: "Male neck-tie",
+        price: 10.99,
+        image: require("../assets/images/tie.png"),
+        description: "Official school tie for all pupils in the school.",
+        rating: 3.9,
+        ratingCount: 24
+    },
+    {
+        productId: "5",
+        name: "Sicker",
+        price: 5.99,
+        image: require("../assets/images/sticker.jpg"),
+        description: "Official school stickers for prefects",
+        rating: 4.8,
+        ratingCount: 5
+    },
+    {
+        productId: "6",
+        name: "Pencil",
+        price: 9.9,
+        image: require("../assets/images/pencil.jpg"),
+        description: "Wooden graphites pencils for primary schools pupils",
+        rating: 4.2,
+        ratingCount: 200
+    },
+]
